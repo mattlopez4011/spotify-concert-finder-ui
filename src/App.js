05de5cecab45c06d71a9aebee4c418b6b66d3964
mattlopez4011 <matthew.lopez4011@gmail.com>
2021-08-23 10:50:06 -0500

    Add gif to test clearing cloudfront cache via Githb actions

--- src/App.js
+++ src/App.js
@@ -18,6 +18,16 @@ function App() {
           Learn React
         </a>
         <h1>Hello! My name is Jeff! 🤣</h1>
+        <br/>
+        <div style={{width: '10em'}}>
+          {/*style="width:100%;height:0;padding-bottom:100%;position:relative;"*/}
+          {/*frameBorder="0"*/}
+          <div style={{width: '100%', height: 0, paddingBottom: '100%', position: 'relative'}} >
+            <iframe style={{width: '100%', height: '100%', position: 'absolute'}} src="https://giphy.com/embed/wGEymBvo6FUlR9bbda"
+     className="giphy-embed" allowFullScreen/>
+          </div>
+          <p><a href="https://giphy.com/gifs/my-code-works-runs-wGEymBvo6FUlR9bbda">via GIPHY</a></p>
+        </div>
       </header>
     </div>
   );
